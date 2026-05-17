746d005352b8f2bbb7d6d6876e461b7f7ba6249c
Pesach Bagad <bgdpsh@gmail.com>
2026-05-17 12:31:20 +0300

    Create new folder of week_6 and add him a new exception.py

---
+++ week_6/exceptions.py
@@ -0,0 +1,61 @@
+# 1
+def safe_int(s):
+    try: return int(s)
+    except (ValueError, TypeError):
+        return None
+# 2
+def safe_divide(a,b):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        return "undefined"
+print(safe_divide(5,3))
+# 3
+def get_value(d,key):
+    try:
+        return d[key]
+    except KeyError:
+        return "missing"
+# 4
+def parse_ints(values):
+    new_lst=[]
+    for item in values:
+        try:
+            new_int=int(item)
+            new_lst.append(new_int)
+        except ValueError:
+            continue
+    return new_lst
+print(parse_ints(['1','34','fssa','5','6']))
+# 5
+def set_age(age):
+    try:
+        if not (150>=age>0):
+            raise("Error age is not valid")
+    except ValueError:
+        raise ValueError("Value error")
+# 6
+def retery(func,n):
+    for index in range(n):
+        try:
+            return func()
+        except Exception:
+            if index == n-1:
+                raise
+# 7
+def error_count(funcs):
+    counter=0
+    for function in funcs:
+        try:
+            function()
+        except Exception as e:
+            counter+=1
+    return counter
+# 8
+def load_config(path):
+    try:
+        with open(file.txt,"r") as f:
+            int(f.readline())
+    except Exception as e:
+        raise RuntimeError("failed to load config") from e
+
